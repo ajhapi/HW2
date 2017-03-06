6aed3a3f94d7177283e969385d752018db8985b5
ajhapi <ajena1993@gmail.com>
2017-03-06 11:04:55 +0530

    Create HelloServlet.java

---
+++ WEB-INF/classes/HelloServlet.java
@@ -0,0 +1,17 @@
+ 
+import java.io.*;  
+  
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+  
+public class HelloServlet extends HttpServlet {  
+ public void doGet (HttpServletRequest req,  
+ HttpServletResponse res)  
+ throws ServletException, IOException  
+ {  
+ PrintWriter out = res.getWriter();  
+  res.setContentType("text/html");
+ out.println("Hello, world!");  
+ out.close();  
+ }  
+}  
